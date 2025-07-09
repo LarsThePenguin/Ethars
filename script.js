102131fb44dd04070613cbcc3a24ebbd52e7f2be
Lars <larsthepenguin@proton.me>
2025-07-09 15:51:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const getCookies = (cookieStr) =>
+  cookieStr.split(";")
+    .map(str => str.trim().split(/=(.+)/))
+    .reduce((acc, curr) => {
+        acc[curr[0]] = curr[1];
+        return acc;
+    }, {})
+
+function checkIfSignedUp() {
+    var cookies = getCookies(document.cookie);
+    var cookiesKeys = Object.keys(cookies);
+    if (cookiesKeys.includes("username") && cookiesKeys.includes("password")) {
+        window.location = "/chat.html";
+    } else {
+        window.location = "/signup.html";
+    }
+}
